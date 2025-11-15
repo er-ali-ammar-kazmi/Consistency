fb455774ab02feece9e33231ebb4bb9faa03f14a
ali-kazmi <aliammarkazmi16@gmail.com>
2025-11-15 17:08:03 +0530

    11/15/25

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	app "practise/application"
-
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-	app.StartRestCalculatorServer()
 }
---
+++ searches/search.go
@@ -0,0 +1,40 @@
+package search
+
+import "fmt"
+
+func LinearSearch(arr []int, target int) (int, bool) {
+	fmt.Println(arr, target)
+	for idx, ele := range arr {
+		if ele == target {
+			return idx, true
+		} else if ele > target {
+			return idx, false
+		}
+	}
+	return -1, false
+}
+
+func BinarySearch(arr []int, target int) (int, bool) {
+	fmt.Println(arr, target)
+	if target > arr[len(arr)-1] {
+		return -1, false
+	}
+
+	var left, mid, right = 0, 0, len(arr) - 1
+
+	for left <= right {
+		mid = (left + right) / 2
+		fmt.Println(left, right, mid)
+
+		if arr[mid] == target {
+			return mid, true
+		}
+
+		if arr[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return mid, false
+}
