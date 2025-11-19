2dd9ac691dbf9e3b5276f50b8a2bead91cc6bba9
ali-kazmi <aliammarkazmi16@gmail.com>
2025-11-19 11:10:23 +0530

    11/19/25

---
+++ sorting/mergeSort.go
@@ -0,0 +1,36 @@
+package sorting
+
+import "fmt"
+
+// O(nlogn)
+func MergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+
+	left := MergeSort(arr[:mid])
+	right := MergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0)
+
+	var i, j int
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	fmt.Println(result)
+	return result
+}
